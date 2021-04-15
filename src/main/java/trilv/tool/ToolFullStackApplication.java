93b4c3205ddbe623a6e078f56c66799913c701e0
vantri0964 <vantri27051998@gmail.com>
2021-04-15 22:38:18 +0700

    first commit

---
+++ src/main/java/trilv/tool/ToolFullStackApplication.java
@@ -0,0 +1,13 @@
+package trilv.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trilv/tool/ToolFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package trilv.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
