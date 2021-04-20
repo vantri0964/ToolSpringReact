e2895f95ddbbd83b9e2104ae453373335d4feefc
vantri0964 <vantri27051998@gmail.com>
2021-04-20 22:32:51 +0700

    commit branch 2 validation

--- src/main/java/trilv/tool/controller/ProjectController.java
+++ src/main/java/trilv/tool/controller/ProjectController.java
@@ -1,8 +1,11 @@
 package trilv.tool.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +22,11 @@ public class ProjectController {
 	private ProjectService projectService;
 
 	@PostMapping("")
-	public ResponseEntity<Project> saveProject(@RequestBody Project project) {
+	public ResponseEntity<?> saveProject(@Valid @RequestBody Project project, BindingResult error) {
+	    if(error.hasErrors()) {
+	        return new ResponseEntity<String>("Invalid project object", HttpStatus.BAD_REQUEST);
+	    }
+	    
 		Project result = projectService.saveProject(project);
 		return new ResponseEntity<Project>(result, result != null ? HttpStatus.ACCEPTED : HttpStatus.FAILED_DEPENDENCY);
 	}
